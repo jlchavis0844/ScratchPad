569dfb56db01db644d11f9d304000ff6001b2d32
jchavis <jchavis@DBA.ea.corp.com>
2017-11-13 14:22:41 -0800

    Added new owner and source fetching to postLeads.py

--- ScratchPad/postLead.py
+++ ScratchPad/postLead.py
@@ -14,29 +14,8 @@ import tkinter  # for the file picker
 import tkinter.filedialog as fd  # for picking the file
 from datetime import datetime  # for comparing time stamps
 
-
-
-# owners list will hold Key = Name, Value = owner_id
-owners = {
-    "Robert Lotter"  :768466, "Christa Colton"  :770197, "Steven Chow"  :770199, "Sheila Tedtaotao"  :770211,
-    "Data Admin"  :770454, "David Barto"  :774967, "Kristi Breiten"  :775616, "Felipe Diaz"  :775617,
-    "Chris Coventry"  :775618, "David Coventry"  :775619, "Rachel Lane"  :775621, "Darren Hulbert"  :775625,
-    "Kusuma Suharto"  :775633, "Royce Meredith"  :775638, "Joyce Lavin"  :775768, "Chris McFarland"  :775769,
-    "Victor Leonino"  :778823, "Chuck Major"  :783519, "Paul Bruil"  :799111, "Adrian Galvan"  :800987,
-    "Duane Kelley"  :813489, "Frank McDermott"  :813490, "Marketing Team"  :825798,
-    "newbusiness@ralotter.com"  :828713, "Van Castaneda"  :861053, "Jonathan Pace"  :940653,
-    "Gina Gonzales"  :966741, "Dominic Fama"  :973096, "Horacio Rojas"  :977350, "Brian Mendenhall"  :977351,
-    "James Chavis"  :991960, "Troy Mathis"  :1000138, "Guillermo Olmedo"  :1000159, "Katherine Seach"  :1000169
-    }
-
-sources = {"Direct Mail" : 118573, "Telemarketing" : 118574, "TDS Compliance Clinic" : 118587, "TDS Retired" : 118588,
-           "Referral" : 118590, "Guest" : 118592, "FHRI Agent Referral" : 118593, "TDS Referral" : 118594, 
-           "TDS Web" : 118595,"Email" : 118948, "TDS MN Print" : 118949, "TDS MN Email" : 118950, "TDS Audit" : 118951, 
-           "TDS 125 Open Enrollment" : 118952,"None" : 137256, "FHRI My Advisor Magazine" : 150539, 
-           "Direct Mail Phone-In" : 150540, "Beyond" : 159032, "CareerBuilder" : 159033,"Monster" : 159034, 
-           "Active Sale" : 184862, "Indeed Ad" : 277038, "Craigs List" : 284440, "Campaign Referral" : 351441,
-           "TDS Customer" : 396033, "Exact Data" : 600804
-           }
+owners = {}
+sources = {}
 
 merged = []
 mergeCount = 0;
@@ -85,20 +64,70 @@ def timeDiff(created, updated):
     # print(str(difference.seconds) + " second time difference")
     return difference.seconds  # one hour is 3600 seconds
 
+def loadOwners():
+    global token
+    global owners
+    if token is None or token == '':
+        token = getToken()
+    
+    oheaders = {'Accept': 'application/json',
+                 'Content-Type': 'application/json',
+                 'Authorization': 'Bearer ' + token}
+    
+    oURL = 'https://api.getbase.com/v2/users?per_page=100&status=active'
+    oresponse = requests.get(url=oURL, headers=oheaders, verify = True)
+    oresponse_json = json.loads(oresponse.text)  # read in the response JSON
+    items = oresponse_json['items']
+    owners = {}
+    
+    for item in items:
+        owners[item['data']['name']] = item['data']['id']
+
+# this function finds the owner id for the given name and returns owner_id as an int
+def getOwner(ownerName):
+    global owners
+    if not owners or len(owners) == 0:
+        loadOwners()
+    
+    if(ownerName == "" or ownerName == " " or ownerName is None):
+        return ""
+    else:
+        return owners[ownerName]    
+
 # serves as a getter for source that reads sources by the key and returns value
 # all performs blank, space, and null check (returns empty)
+#if sources not loaded, it will load them via api
 def getSource(sourceName):
+    global sources
+    if not sources or len(sources) == 0:
+        loadSources()
+        
     if(sourceName == "" or sourceName == " " or sourceName is None):
         return ""
     else:
         return sources[sourceName]
 
-# this function finds the owner id for the given name and returns owner_id as an int
-def getOwner(ownerName):
-    if(ownerName == "" or ownerName == " " or ownerName is None):
-        return ""
-    else:
-        return owners[ownerName]
+#fetches the inital sources list from the baseAPI. This will only load the first 100
+#currently set to use global, could comment it out and simple return source for sources = loadSources()
+#use as loadSources() with the sources loaded into global sources variable as dict
+def loadSources():
+    global token
+    if token is None or token == '':
+        token = getToken()
+    
+    sheaders = {'Accept': 'application/json',
+                 'Content-Type': 'application/json',
+                 'Authorization': 'Bearer ' + token}
+    
+    sURL = "https://api.getbase.com/v2/lead_sources?per_page=100"
+    sresponse = requests.get(url=sURL, headers=sheaders, verify=True)  # send it
+    sresponse_json = json.loads(sresponse.text)  # read in the response JSON
+    items = sresponse_json['items']
+    global sources
+    sources = {}
+    for item in items:
+        sources[item['data']['name']] = item['data']['id']
+ 
     
 # optional stuff for the file picker
 FILEOPENOPTIONS = dict(defaultextension='.csv', filetypes=[('CSV file', '*.csv'), ('All files', '*.*')])
@@ -138,57 +167,52 @@ with open(csv_path, encoding="utf8", newline='', errors='ignore') as csvfile:  #
         tagVal = [] # the array to put into data as an array of tags
         
         #Lets load the CSV values into JSON objects data, address, or custom
-        data["email"] = row[0]  # each column is mapped to a json object 
+        data["email"] = row[10]  # each column is mapped to a json object 
         data["source_id"] = getSource(row[1])
-        custom_fields["Our Region"] = row[2]
-        custom_fields["TDS"] = row[3]
-        custom_fields["Territory"] = row[4]
-        data["owner_id"] = getOwner(row[5])
-        data["first_name"] = row[6]
-        data["last_name"] = row[7]
-        address["line1"] = row[8]
-        address["city"] = row[9]
-        address["state"] = row[10]
-        address["postal_code"] = row[11]
-        custom_fields["Home Phone"] = row[12]
-        data["mobile"] = row[13]
-        custom_fields["NDNC"] = row[14]
-        custom_fields["NDNC HmPhone"] = row[15]
-        custom_fields["NDNC ExpDate"] = row[16]
-        custom_fields["Worksite"] = row[17]
-        custom_fields["Worksite Phone"] = row[18]
-        custom_fields["Worksite Email"] = row[19]
-        custom_fields["District"] = row[20]
-        custom_fields["District Phone"] = row[21]
-        custom_fields["Alt Phone"] = row[22]
-        custom_fields["Business Phone"] = row[23]
-        custom_fields["Business Phone Ext"] = row[24]
-        custom_fields["DOB"] = row[25]
-        custom_fields["Gender"] = row[26]
-        custom_fields["Annual Salary"] = row[27]
-        custom_fields["YrsPERS"] = row[28]
-        custom_fields["YrsSTRS"] = row[29]
-        custom_fields["Planned Retire"] = row[30]
-        custom_fields["Beneficiary"] = row[31]
-        custom_fields["Beneficiary DOB"] = row[32]
-        custom_fields["Spouse Name"] = row[33]
-        custom_fields["Mktg ID"] = row[34]
-        custom_fields["New Lead Type"] = row[35]
+        custom_fields["Our Region"] = row[35]
+        custom_fields["TDS"] = row[34]
+        custom_fields["Territory"] = row[11]
+        data["owner_id"] = getOwner(row[0])
+        data["first_name"] = row[2]
+        data["last_name"] = row[3]
+        address["line1"] = row[4]
+        address["city"] = row[5]
+        address["state"] = row[6]
+        address["postal_code"] = row[7]
+        custom_fields["Home Phone"] = row[8]
+        data["mobile"] = row[9]
+        custom_fields["NDNC"] = row[12]
+        custom_fields["NDNC HmPhone"] = row[13]
+        custom_fields["NDNC ExpDate"] = row[14]
+        custom_fields["Worksite"] = row[15]
+        custom_fields["Worksite Phone"] = row[16]
+        custom_fields["Worksite Email"] = row[17]
+        custom_fields["District"] = row[18]
+        custom_fields["District Phone"] = row[19]
+        custom_fields["Alt Phone"] = row[20]
+        custom_fields["Business Phone"] = row[21]
+        custom_fields["Business Phone Ext"] = row[22]
+        custom_fields["DOB"] = row[23]
+        custom_fields["Gender"] = row[24]
+        custom_fields["Annual Salary"] = row[25]
+        custom_fields["YrsPERS"] = row[26]
+        custom_fields["YrsSTRS"] = row[27]
+        custom_fields["Planned Retire"] = row[28]
+        custom_fields["Beneficiary"] = row[29]
+        custom_fields["Beneficiary DOB"] = row[30]
+        custom_fields["Spouse Name"] = row[31]
+        custom_fields["Mktg ID"] = row[32]
+        custom_fields["New Lead Type"] = row[33]
         custom_fields["New Section"] = row[36]
         custom_fields["Client ID"] = row[37]
         custom_fields["Agent ID"] = row[38]
         custom_fields["Response Note"] = row[39]
         
-        # add the tags as a JSON array and upserting seems to clear all Tags for some reason
-        #TODO: fix tag JSON array not working on upsert, implement tag update script logic using import tagUpdate
+        #add the tags as a JSON array 
+        #TODO: fix tag JSON array not working on
         if(row[40] != "" or row[40] is None):
             tagVal.append(row[40]) # read in value of tag ( for not just one value)
-            data['tags'] = tagVal # write array to json field data : {...'tags' : ['value']...}      
-        #################################################### WARNING: ADDED FOR SUMMER AUDITS ###########
-            data['tags'].append("Summer Audits")
-        else:
-            data['tags'] = ["Summer Audits"]
-        #################################################### WARNING: ADDED FOR SUMMER AUDITS ###########
+            data['tags'] = tagVal # write array to json field data : {...'tags' : ['value']...}
         
         #------------------------------------Check for empty rows--------------------------------------
         # if a lead does not have a first and a list name, count this as a blank row and skip
@@ -230,24 +254,10 @@ with open(csv_path, encoding="utf8", newline='', errors='ignore') as csvfile:  #
         headers = {'Accept': 'application/json',
                  'Content-Type': 'application/json',
                  'Authorization': 'Bearer ' + token}
-        
-        url = ""  # decide how the leads will merge on upsert if needed
-#         if "Worksite" in custom_fields and "District" in custom_fields:  # if Worksite is not empty
-#             url = "https://api.getbase.com/v2/leads/upsert?first_name=" + data["first_name"] + "&last_name="\
-#             + data["last_name"] + "&custom_fields[District]=" + custom_fields["District"] + "&custom_fields[Worksite]="\
-#             + custom_fields["Worksite"]
-#         elif "Worksite" in custom_fields:# if there is no district but there is a worksite 
-#             url = "https://api.getbase.com/v2/leads/upsert?first_name=" + data["first_name"] + "&last_name="\
-#             + data["last_name"] + "&custom_fields[Worksite]=" + custom_fields["Worksite"]
-#         elif "District" in custom_fields.keys():  # if Worksite is empty, but not district
-#             url = "https://api.getbase.com/v2/leads/upsert?first_name=" + data["first_name"] + "&last_name="\
-#             + data["last_name"] + "&custom_fields[District]=" + custom_fields["District"]
-#         else:  # if Worksite and district is empty, just first and last name
-#             url = "https://api.getbase.com/v2/leads/upsert?first_name=" + data["first_name"] + "&last_name="\
-#             + data["last_name"]
-#       
+
+
         url = "https://api.getbase.com/v2/leads"   
-        file.write("URI: " + url + '\n')  # write the URI to the log file
+        #file.write("URI: " + url + '\n')  # write the URI to the log file
         response = requests.post(url=url, headers=headers, data=json.dumps(payload), verify=True)  # send it
         response_json = json.loads(response.text)  # read in the response JSON
         
@@ -258,38 +268,7 @@ with open(csv_path, encoding="utf8", newline='', errors='ignore') as csvfile:  #
             newBaseID = response_json['data']['id']
             print('Base ID: ' + str(newBaseID) + '\n')
             file.write('Base ID: ' + str(newBaseID) + '\n')
-#         created = response_json['data']['created_at']  # store the time stamp for when the lead was created
-#         updated = response_json['data']['updated_at']  # store the time stamp for when the lead was updated
-#         file.write("created: " + created + '\n')  # write the created time stamp to log
-#         file.write("updated: " + updated + '\n')  # write the updated time stamp to the log
-#         
-#         if(timeDiff(created, updated) > 60):  # if the created and updated are more than a minute apart
-#             print("****WARNING, THIS LEAD WAS MERGED*****" + '\n')
-#             file.write("****WARNING, THIS LEAD WAS MERGED*****" + '\n')
-#             mergeCount += 1
-#             thisMerge = {}  # build array of merged leads
-#             thisMerge['BaseID'] = response_json['data']['id']
-#             thisMerge['Client ID '] = response_json['data']['custom_fields']['Client ID']
-#             thisMerge['First'] = response_json['data']['first_name']
-#             thisMerge['Last'] = response_json['data']['last_name']
-#             print(list(thisMerge.keys()))
-#             print(list(thisMerge.values()))
-#             print("********************END MERGE DATA**********************\n")
-#             merged.append(thisMerge)  # add to merged list
             
-# # write merged to the log file
-# file.write("the following leads were merged\n")        
-# for item in merged:
-#     file.write(str(item) + '\n')
-
-# make a special CSV of the merges 
-# if(mergeCount > 0):  # if merges were detected
-#     print
-#     with open(os.getcwd() + '\\mergeReport_' + time + '.csv', 'w') as f:  # open csv
-#         w = csv.DictWriter(f, merged[0].keys(), lineterminator='\n')  # make writer
-#         w.writeheader()  # write header row
-#         for merge in merged:  # write the merges to the merge CSV
-#             w.writerow(merge)
 
 file.close
 input("press Enter key to continue") # pause at the end of running the program to allow for reading
